11dc17e1f684d5e88324764935aed101dddc7be5
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-23 10:35:29 +0000

    separates view and controller

--- app.rb
+++ app.rb
@@ -11,16 +11,5 @@ get '/secret' do
 end
 
 get '/cat' do
-    "<div style=
-    'border: 0.5rem outset pink;
-    outline: 0.5rem solid khaki;
-    box-shadow: 0 0 0 2rem skyblue;
-    border-radius: 12px;
-    font: bold 1rem sans-serif;
-    margin: 2rem;
-    padding: 1rem;
-    outline-offset: 0.5rem;
-    '>
-      <img src='http://bit.ly/1eze8aE'>
-   </div>"
+    erb :index
 end
