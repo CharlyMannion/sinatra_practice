ec2357e2d5c3e2bf92a976e937ac937b197b8af8
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-23 11:46:42 +0000

    adds named_cat route that uses params

--- app.rb
+++ app.rb
@@ -10,9 +10,16 @@ get '/secret' do
   'Secret whisper'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb :index
 end
 
-# http://localhost:4567/cat
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
+end
+
+# http://localhost:4567/random-cat
+# http://localhost:4567/named-cat?name=James
