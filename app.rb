d877bdcbf1f515f7683ab104e021f82b73871968
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-23 10:27:35 +0000

    creates cat route with html and css

--- app.rb
+++ app.rb
@@ -10,6 +10,17 @@ get '/secret' do
   'Secret whisper'
 end
 
-get '/cats' do
-  'kitty kitty'
+get '/cat' do
+    "<div style=
+    'border: 0.5rem outset pink;
+    outline: 0.5rem solid khaki;
+    box-shadow: 0 0 0 2rem skyblue;
+    border-radius: 12px;
+    font: bold 1rem sans-serif;
+    margin: 2rem;
+    padding: 1rem;
+    outline-offset: 0.5rem;
+    '>
+      <img src='http://bit.ly/1eze8aE'>
+   </div>"
 end
